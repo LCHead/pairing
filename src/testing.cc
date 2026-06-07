96d0e0f80fd458de62de759d0a38601c2d5ac4d4
LCHead <71817179+LCHead@users.noreply.github.com>
2026-06-07 00:06:54 -0400

    added vector container

--- src/testing.cc
+++ src/testing.cc
@@ -12,10 +12,19 @@ using namespace std;
 int main(){
 
     // Testing players
-    Player joe1 = Player("Joe","Bloggs","M",1990,2000.0,"USA");
-    Player joe2 = Player("Joe","Bloggs","M",1990,2000.0,"USA");
-    Player joe3 = Player("Joe","Bloggs","M",1990,2000.0,"USA");
-    Player joe4 = Player("Joe","Bloggs","M",1990,2000.0,"USA");
+    vector<Player> playerList;
+    playerList.push_back(Player("Joe","Bloggs","M",1990,2000.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,1900.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,1500.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,2100.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,2200.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,2100.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,1600.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,2500.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,2000.0,"USA"));
+    playerList.push_back(Player("Joe","Bloggs","M",1990,1950.0,"USA"));
+
+    cout << playerList.size() << endl;
 
     // Find total number of players
     cout << "Number of players = " << Player::getPlayerCount() << endl;
